031995a0f7955b6f68ac0c5f1f7e40c5b69638ad
Luke Koko <lukekoko1@gmail.com>
2019-04-16 19:46:45 +1000

    Cleaning up code, fixing html formatting

--- db.py
+++ db.py
@@ -1,15 +1,16 @@
 import sqlite3
 import bcrypt
 
-def setup(conn):
-	print("setup")
-	conn.execute("DROP TABLE IF EXISTS user;")
-	conn.execute("DROP TABLE IF EXISTS ads")
-	# conn.execute("DROP TABLE IF EXISTS ads_history")
-	conn.execute("DROP TABLE IF EXISTS booking")
-	# conn.execute("DROP TABLE IF EXISTS booking_history")
 
-	conn.execute("""
+def setup(conn):
+    print("setup")
+    conn.execute("DROP TABLE IF EXISTS user;")
+    conn.execute("DROP TABLE IF EXISTS advertisements")
+    conn.execute("DROP TABLE IF EXISTS booking")
+    # conn.execute("DROP TABLE IF EXISTS ads_history")
+    # conn.execute("DROP TABLE IF EXISTS booking_history")
+
+    conn.execute("""
 		CREATE TABLE IF NOT EXISTS user
 		(
 			id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -22,48 +23,55 @@ def setup(conn):
 		);
 	""")
 
-	conn.execute("""
-		CREATE TABLE IF NOT EXISTS ads
+    conn.execute("""
+		CREATE TABLE IF NOT EXISTS advertisements
 		(
 			id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 			title TEXT NOT NULL,
 			description TEXT,
+			active INT,
 			user_id INT,
 			FOREIGN KEY (user_id) REFERENCES user(id)
 		);
 	""")
 
-	conn.execute("""
+    conn.execute("""
 		CREATE TABLE IF NOT EXISTS bookings
 		(
 			id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 			title TEXT NOT NULL,
 			description TEXT,
 			user_id INT,
+			active INT,
 			Location TEXT,
 			FOREIGN KEY (user_id) REFERENCES user(id)
 		)
 	""")
 
-	# password = bcrypt.hashpw('1234', bcrypt.gensalt())
-	# conn.execute("INSERT INTO user (name, email, password, type) VALUES ('test', 'test@gmail.com', ?, 1)", (password,))
+    # password = bcrypt.hashpw('1234', bcrypt.gensalt())
+    # conn.execute("INSERT INTO user (name, email, password, type) VALUES ('test', 'test@gmail.com', ?, 1)", (password,))
+
 
 def registerUser(conn, user):
-	password = user['password']
-	user['password'] = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
-	try:
-		conn.execute("INSERT INTO user (name, email, password, type) VALUES (:name, :email, :password, :usertype)", user)
-		return True
-	except:
-		return False
-		
+    password = user['password']
+    user['password'] = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+    try:
+        conn.execute(
+            "INSERT INTO user (name, email, password, type) VALUES (:name, :email, :password, :usertype)", user)
+        return True
+    except:
+        return False
+
+
 def checkPassword(conn, email, password):
-	try:
-		hashPW = conn.execute("SELECT password FROM user WHERE email=?", (email,)).fetchone()
-		if bcrypt.checkpw(password.encode('utf8'), hashPW[0].encode('utf8')):
-			usertype = conn.execute("SELECT type FROM user WHERE email=?", (email,)).fetchone()
-			return usertype[0]
-		else:
-			return False
-	except: 
-		return False
+    try:
+        hashPW = conn.execute(
+            "SELECT password FROM user WHERE email=?", (email,)).fetchone()
+        if bcrypt.checkpw(password.encode('utf8'), hashPW[0].encode('utf8')):
+            usertype = conn.execute(
+                "SELECT type FROM user WHERE email=?", (email,)).fetchone()
+            return usertype[0]
+        else:
+            return False
+    except:
+        return False
--- site.py
+++ site.py
@@ -16,23 +16,6 @@ def dummy():
 def homePage(response):
     response.write(TemplateAPI.render('homepage.html', response, {}))
 
-def login(response):
-    response.write(TemplateAPI.render('login.html', response, {}))
-
-def loginPost(response):
-    email = response.get_field("email")
-    password = response.get_field("password")
-    matches = db.checkPassword(dbConn, email, password)
-    usertype = matches
-    print(usertype)
-    if matches:
-        profile(response)
-    else:
-        login(response)
-
-def profile(response):
-    response.write(TemplateAPI.render('profile.html', response, {}))    
-
 def register(response):
     response.write(TemplateAPI.render('register.html', response, {}))
 
@@ -43,10 +26,39 @@ def registerPost(response):
     user['password'] = response.get_field("password1")
     user['usertype'] = response.get_field("usertype")
     registered = db.registerUser(dbConn, user)
+    # register successful go to homepage/login
     if registered:
         homePage(response)
+    # register failed go back to register
     else:
         register(response)
+
+def login(response):    
+    response.write(TemplateAPI.render('login.html', response, {}))
+
+def loginPost(response):
+    email = response.get_field("email")
+    password = response.get_field("password")
+    matches = db.checkPassword(dbConn, email, password)
+    global usertype
+    # Charity
+    if matches == 1:
+        usertype = "Charity"
+        # print usertype
+        dashboard(response)
+    # Donor
+    elif matches == 2:
+        usertype = "Donor"
+        dashboard(response)
+    # Wrong password or no user exists go back to login
+    else:
+        login(response)
+
+def dashboard(response):
+    if usertype:
+        response.write(TemplateAPI.render('dashboard.html', response, {}))  
+    else:
+        response.write("No access to this page")  
         
 def randPage(response):
     response.write("Today's Random number is: " + str(ranGen()))
@@ -61,5 +73,6 @@ server.register('/login', login)
 server.register('/login/post', loginPost)
 server.register('/register', register)
 server.register('/register/post', registerPost)
+server.register('/dashboard', dashboard)
 
 server.run(dummy)
