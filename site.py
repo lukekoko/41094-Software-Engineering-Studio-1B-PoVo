ba83e1ce4fcb5515e2ef345919f34c09c1ae310f
lukekoko <lukekoko1@gmail.com>
2019-04-28 18:14:35 +1000

    Templated advertisement page

--- site.py
+++ site.py
@@ -96,8 +96,9 @@ def dashboard(response):
 
 @loginCheck
 def advertisement(response):
+    items = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
     response.write(TemplateAPI.render("advertisement.html",
-                                      response, {"title": "Advertisement"}))
+                                      response, {"title": "Advertisement", "items": items}))
 
 
 def main():
