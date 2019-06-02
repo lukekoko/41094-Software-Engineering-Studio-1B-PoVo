eba4aaadb78b1f6740feb945ab0a9b498f20e99c
KeyserSoz3 <kevinguemail@gmail.com>
2019-06-02 20:24:31 +1000

    UI changes

--- db.py
+++ db.py
@@ -2,17 +2,20 @@ import sqlite3
 import bcrypt
 import os
 
+
 def registerUser(conn, user):
     password = user['password']
+    user['active'] = 0
     user['password'] = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
     try:
         conn.execute(
-            "INSERT INTO user (name, email, password, type) VALUES (:name, :email, :password, :usertype)", user)
+            "INSERT INTO user (name, email, password, type, active) VALUES (:name, :email, :password, :usertype, :active)", user)
         conn.commit()
         return 1
     except sqlite3.IntegrityError:
         return 2
     except Exception:
+        print "user registration exception"
         return 3
 
 
@@ -22,7 +25,7 @@ def resetUserPassword(conn, user):
         user['password'] = bcrypt.hashpw(
             password.encode('utf8'), bcrypt.gensalt())
         conn.execute(
-            "UPDATE user set password = :password WHERE email= :email", user)
+            "UPDATE user set password = :password WHERE id= :id", user)
         return True
     except:
         return False
@@ -34,7 +37,7 @@ def checkPassword(conn, email, password):
             "SELECT password FROM user WHERE email=?", (email,)).fetchone()
         if bcrypt.checkpw(password.encode('utf8'), hashPW[0].encode('utf8')):
             usertype = conn.execute(
-                "SELECT id, type, name FROM user WHERE email=?", (email,)).fetchone()
+                "SELECT id, type, name, active FROM user WHERE email=?", (email,)).fetchone()
             return usertype
         else:
             return False
@@ -56,61 +59,62 @@ def createAd(conn, ad):
         return False
 
 
-def getAds(conn):
+def getAds(conn, search):
     ads = []
     cursor = conn.execute(
-        "SELECT id, title, description, datetime, user_id FROM advertisements")
+        "SELECT id, title, description, datetime, user_id FROM advertisements WHERE title LIKE ?", (search,))
     for row in cursor:
-        print row
         ad = {}
         ad["id"] = row[0]
         ad["title"] = row[1]
         ad["desc"] = row[2]
         ad["datetime"] = row[3]
-        ad["user_id"] = row[4]
+        ad["user_id"] = int(row[4])
         try:
             imgpath = conn.execute(
                 "SELECT path FROM advertisement_img WHERE ad_id=?", (row[0],))
             ad["img_path"] = imgpath.fetchone()[0]
         except:
             ad["img_path"] = ""
-        # print ad
         ads.append(ad)
     return ads
 
 
-def deleteAds(conn, id,logged_in_user):
+def deleteAds(conn, id):
     try:
-        user_id = conn.execute(
-            "SELECT user_id FROM advertisements WHERE id=?", (id,)
+        conn.execute(
+            "DELETE FROM advertisements WHERE id=?", (id,)
         )
-        if user_id == logged_in_user :
-            conn.execute(
-                "DELETE FROM advertisements WHERE id=?", (id,)
-            )
-            cursor = conn.execute(
-                "SELECT path FROM advertisement_img WHERE ad_id=?", (id,))
-            for x in cursor:
-                os.remove(x[0])
-            conn.execute(
-                "DELETE FROM advertisement_img WHERE ad_id=?", (id,)
-            )
-            conn.commit()
-            return True
-    except:
-        return False
-
-def createBooking(conn, booking):
-    try:
-        print "cursor conn"
-        cursor = conn.cursor()
-        print "executing cursor"
-        cursor.execute(
-            "INSERT INTO bookings (title, description, datetime, charity_user_id, donor_user_id, active, location) VALUES (:title, :desc, :datetime, :charityuserid, :donoruserid, :active, :location)", booking)
+        cursor = conn.execute(
+            "SELECT path FROM advertisement_img WHERE ad_id=?", (id,))
+        for x in cursor:
+            os.remove(x[0])
+        conn.execute(
+            "DELETE FROM advertisement_img WHERE ad_id=?", (id,)
+        )
         conn.commit()
         return True
     except:
         return False
+
+
+def editAd(conn, ad):
+    conn.execute(
+        "UPDATE advertisements set title = :title, description = :desc WHERE id =:id", ad
+    )
+    conn.commit()
+
+
+def createBooking(conn, booking):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO bookings (title, description, datetime, active, location, user_id) VALUES (:title, :desc, :datetime, :active, :location, :user_id)", booking)
+        conn.commit()
+        return True
+    except:
+        return False
+
 
 def getUser(conn, id):
     try:
@@ -123,20 +127,22 @@ def getUser(conn, id):
 
 
 def editUser(conn, user):
-	try:
-		conn.execute(
-			"UPDATE user set name = :name, email = :email, type = :usertype WHERE id= :id", user)
-		return True
-	except:
-		return False
+    try:
+        conn.execute(
+            "UPDATE user set name = :name, email = :email, type = :usertype WHERE id= :id", user)
+        # conn.commit()
+        return True
+    except:
+        return False
+
 
 def getUserAds(conn, userid):
     ads = []
     cursor = conn.execute(
-        "SELECT id, title, description, datetime, user_id FROM advertisements WHERE user_id=?", userid
+        "SELECT id, title, description, datetime, user_id FROM advertisements WHERE user_id=?", (
+            userid,)
     )
     for row in cursor:
-        print row
         ad = {}
         ad["id"] = row[0]
         ad["title"] = row[1]
@@ -151,3 +157,84 @@ def getUserAds(conn, userid):
             ad["img_path"] = ""
         ads.append(ad)
     return ads
+
+
+def viewAd(conn, id):
+    ads = []
+    cursor = conn.execute(
+        "SELECT id, title, description, datetime, user_id FROM advertisements WHERE id=?", (
+            id,)
+    )
+    for row in cursor:
+        ad = {}
+        ad["id"] = row[0]
+        ad["title"] = row[1]
+        ad["desc"] = row[2]
+        ad["datetime"] = row[3]
+        ad["user_id"] = int(row[4])
+        try:
+            imgpath = conn.execute(
+                "SELECT path FROM advertisement_img WHERE ad_id=?", (row[0],))
+            ad["img_path"] = imgpath.fetchone()[0]
+        except:
+            ad["img_path"] = ""
+        ads.append(ad)
+    return ads
+
+
+def getCharities(conn, sType):
+    try:
+        charities = conn.execute(
+            "SELECT id, name, email FROM user WHERE type=?", (sType,)).fetchall()
+        return charities
+    except:
+        return False
+
+
+def getFilteredCharities(conn, searchQuery, sType):
+    print searchQuery
+    query = "%" + searchQuery + "%"
+    try:
+        charities = conn.execute(
+            "SELECT id, name, email FROM user WHERE type=? AND name LIKE ?", (sType, query,)).fetchall()
+        print charities
+        return charities
+    except:
+        return False
+    
+def confirmUser(conn, email):
+    cursor = conn.execute(
+    "UPDATE user  set active =? WHERE email=?", (1, email)
+    )
+    return 1
+
+def getAppointments(conn, userid):
+    apps = []
+    cursor = conn.execute(
+        "SELECT id, title, description, datetime, location FROM bookings WHERE id=?", (userid,)
+    )
+    for row in cursor:
+        print row
+        ad = {}
+        ad["id"] = row[0]
+        ad["title"] = row[1]
+        ad["desc"] = row[2]
+        ad["datetime"] = row[3]
+        ad["location"] = row[4]
+        apps.append(ad)
+    return apps
+def editApp(conn, app):
+    conn.execute(
+        "UPDATE bookings set title = :title, description = :desc, datetime = :datetime, location = :location WHERE id =:id", app
+    )
+    conn.commit()
+
+def deleteApp(conn, id):
+    try:
+        conn.execute(
+            "DELETE FROM bookings WHERE id=?", (id,)
+        )
+        conn.commit()
+        return True
+    except:
+        return False
--- site.py
+++ site.py
@@ -7,6 +7,7 @@ import sqlite3
 import mailPoVo
 import datetime
 import uuid
+import base64
 
 dbConn = sqlite3.connect('./db/povo.db')
 usertype = ''
@@ -17,7 +18,6 @@ port = 80
 def setup():
     print "Server listening on http://{host}:{port}".format(
         host=host, port=port)
-   # db.setup(dbConn)
 
 
 def loginCheck(fn):
@@ -30,12 +30,13 @@ def loginCheck(fn):
 
 
 def homePage(response):
-    ads = db.getAds(dbConn)
+    ads = db.getAds(dbConn, "%")
+    userid = response.get_secure_cookie("user_id")
     if response.get_secure_cookie('user_id'):
         response.redirect('/dashboard')
     else:
         response.write(TemplateAPI.render(
-            'homepage.html', response, {"title": "Homepage", "ads": ads}))
+            'homepage.html', response, {"title": "Homepage", "ads": ads, "userid": userid}))
 
 
 def register(response):
@@ -55,6 +56,7 @@ def registerPost(response):
     user['usertype'] = response.get_field("usertype")
     register = db.registerUser(dbConn, user)
     # register successful go to homepage/login
+    print register
     if register == 1:
         print "registration succesful"
         mailPoVo.sendConfirmationEmail(user['email'])
@@ -87,6 +89,7 @@ def loginPost(response):
         response.set_secure_cookie('user_id', str(matches[0]))
         response.set_secure_cookie('user_type', str(matches[1]))
         response.set_secure_cookie('name', str(matches[2]))
+        response.set_secure_cookie('active', str(matches[3]))
         response.redirect('/dashboard')
     else:
         response.redirect('/login?fail=1')
@@ -94,14 +97,15 @@ def loginPost(response):
 
 @loginCheck
 def resetPassword(response):
+    usertype = response.get_secure_cookie('user_type')    
     response.write(TemplateAPI.render('resetPassword.html',
-                                      response, {'title': 'Reset Password'}))
+                                      response, {'title': 'Reset Password', "usertype": usertype}))
 
 
 @loginCheck
 def resetPasswordPost(response):
     user = {}
-    user['email'] = response.get_field("email")
+    user['id'] = response.get_secure_cookie('user_id')
     user['password'] = response.get_field("password")
     passwordReset = db.resetUserPassword(dbConn, user)
     # password reset successful go to dashboard
@@ -113,30 +117,91 @@ def resetPasswordPost(response):
 def logout(response):
     response.clear_cookie('user_id')
     response.clear_cookie('user_type')
-    response.redirect('/login')
+    response.redirect('/')
 
 
 @loginCheck
 def dashboard(response):
-    ads = db.getAds(dbConn)
-    usertype = response.get_secure_cookie('user_type')
+    active = response.get_secure_cookie('active')
+    print active
     name = response.get_secure_cookie('name')
-    response.write(TemplateAPI.render(
-        'dashboard.html', response, {"title": "Dashboard", "usertype": usertype, "ads": ads}))
+    usertype = response.get_secure_cookie('user_type')
+    userid = int(response.get_secure_cookie("user_id"))
+    search = response.get_field('charitySearch', '')
+    print search
+    if search:
+        search = "%" + search + "%"
+    else:
+        search = '%'
+    ads = db.getAds(dbConn, search)
 
+    response.write(TemplateAPI.render(
+        'dashboard.html', response, {"title": "Dashboard", "usertype": usertype, "ads": ads, "userid": userid, "active": active, "name": name}))
 
 @loginCheck
 def advertisement(response):
-    ads = db.getAds(dbConn)
-    user_id = response.get_secure_cookie('user_id')
+    search = response.get_field('search', '')
+    print search
+    usertype = response.get_secure_cookie('user_type')
+    userid = int(response.get_secure_cookie("user_id"))
+    ads = db.getAds(dbConn, '%')
     response.write(TemplateAPI.render("advertisement.html",
-                                      response, {"title": "Advertisement", "ads": ads, "user_id": user_id}))
+                                      response, {"title": "Advertisement", "ads": ads, "userid": userid, "usertype": usertype}))
 
 
 @loginCheck
 def advertisementPost(response):
     print "Creating ads"
     ad = {}
+    ad["title"] = response.get_field("title")
+    ad["desc"] = response.get_field("desc")
+    ad["imgpath"] = []
+    img = response.get_files("img")
+
+    for x in img:
+        imgpath = "static/img_store/ad_img/" + str(uuid.uuid4().hex) + ".png"
+        try:
+            with open(imgpath, 'wb') as img:
+                img.write(str(x[2]))
+            ad["imgpath"].append(imgpath)
+        except:
+            pass
+
+    ad["datetime"] = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
+    ad["userid"] = response.get_secure_cookie('user_id')
+    ad["active"] = 1
+    result = db.createAd(dbConn, ad)
+    if result:
+        print "ad succesfully created"
+        response.redirect("/dashboard")
+    else:
+        print "failed to create ad"
+        response.redirect("/dashboard?fail=1")
+
+
+@loginCheck
+def advertisementDelete(response):
+    result = db.deleteAds(dbConn, response.get_field('id'))
+    if result:
+        response.redirect("/dashboard")
+    else:
+        response.redirect("/dashboard?fail=1")
+
+
+@loginCheck
+def adView(response):
+    adId = response.get_field('id', '')
+    ads = db.viewAd(dbConn, adId)
+    usertype = response.get_secure_cookie('user_type')
+    userid = int(response.get_secure_cookie("user_id"))
+    response.write(TemplateAPI.render(
+        "advertisementView.html", response, {"title": "test", "ads": ads, "userid": userid, "usertype": usertype}))
+
+
+@loginCheck
+def advertisementEdit(response):
+    ad = {}
+    ad["id"] = response.get_field('id', '')
     ad["title"] = response.get_field("title")
     ad["desc"] = response.get_field("desc")
     ad["imgpath"] = []
@@ -153,40 +218,30 @@ def advertisementPost(response):
     ad["datetime"] = datetime.datetime.now().isoformat()
     ad["userid"] = response.get_secure_cookie('user_id')
     ad["active"] = 1
-    result = db.createAd(dbConn, ad)
-    if result:
-        print "ad succesfully created"
-        response.redirect("/dashboard")
-    else:
-        print "failed to create ad"
-        response.redirect("/dashboard?fail=1")
-
-
-def advertisementDelete(response):
-    # print response.get_field('id')
-    result = db.deleteAds(dbConn, response.get_field('id'),response.get_secure_cookie('user_type'))
-    if result  :
-        response.redirect("/dashboard")
-    else:
-        response.redirect("/dashboard?fail=1")
+    db.editAd(dbConn, ad)
+    # view = "/advertisement/view?id=%s" % ad["id"]
+    response.redirect('/dashboard')
 
 
 @loginCheck
 def booking(response):
+    usertype = response.get_secure_cookie('user_type')
     response.write(TemplateAPI.render(
-        "booking.html", response, {"title": "Booking"}))
+        "booking.html", response, {"title": "Booking", "usertype": usertype}))
 
 
 @loginCheck
 def bookingPost(response):
+    adId = response.get_field('id', '')
+
     booking = {}
     booking["title"] = response.get_field("title")
     booking["desc"] = response.get_field("desc")
     booking["datetime"] = response.get_field("datetime")
-    booking["charityuserid"] = 1
-    booking["donoruserid"] = 1
+    booking["user_id"] = response.get_secure_cookie('user_id')
     booking["active"] = 1
     booking["location"] = response.get_field("location")
+    booking["ad_id"] = adId
     print booking
     print "attempting to create booking"
     result = db.createBooking(dbConn, booking)
@@ -197,14 +252,14 @@ def bookingPost(response):
         print "Failed to create booking"
         response.redirect("/booking")
 
-
 @loginCheck
 def manageAccount(response):
     userid = response.get_secure_cookie('user_id')
+    fail = response.get_field('fail', '') == '1'
     user = db.getUser(dbConn, userid)
-    print user
+    usertype = response.get_secure_cookie('user_type')
     response.write(TemplateAPI.render("manageAccount.html",
-                                      response, {"title": "Account", "user": user}))
+                                      response, {"title": "Account", "user": user, "fail":  fail, "usertype": usertype}))
 
 
 @loginCheck
@@ -218,17 +273,73 @@ def editAccount(response):
     if result:
         response.redirect("/account")
     else:
-        response.redirect("/account")
+        response.redirect("/account?fail=1")
 
 
 @loginCheck
 def userAds(response):
     ads = db.getUserAds(dbConn, response.get_secure_cookie('user_id'))
+    usertype = response.get_secure_cookie('user_type')
     print ads
     response.write(TemplateAPI.render(
-        "userAds.html", response, {"title": "My Ads", "ads": ads}))
+        "userAds.html", response, {"title": "My Ads", "usertype": usertype, "ads": ads}))
 
 
+@loginCheck
+def searchCharities(response):
+    usertype = response.get_secure_cookie('user_type')
+    sType = 0
+    if int(usertype) == 1:
+        sType = 2
+    elif int(usertype) == 2:
+        sType = 1
+
+    if not response.get_field("charitySearch"):
+        charities = db.getCharities(dbConn, sType)
+        response.write(TemplateAPI.render("searchCharities.html",
+                                          response, {"title": "Charities", "charities": charities, "usertype": usertype}))
+    else:
+        searchQuery = response.get_field("charitySearch")
+        filteredCharities = db.getFilteredCharities(dbConn, searchQuery, sType)
+        response.write(TemplateAPI.render("filterCharities.html",
+                                          response, {"title": "Charities", "filteredCharities": filteredCharities, "usertype": usertype}))
+
+def confirmation(response):
+    print "does this work"
+    email = base64.b64decode(response.get_field('acc', ''))
+    response.clear_cookie('active')
+    response.set_secure_cookie('active', str(db.confirmUser(dbConn, email)))
+    print db.confirmUser(dbConn, email)
+    print response.get_secure_cookie('active')
+    response.redirect("/")
+    
+@loginCheck
+def viewAppointments(response):
+    usertype = response.get_secure_cookie('user_type')
+    apps = db.getAppointments(dbConn, response.get_secure_cookie('user_id'))
+    response.write(TemplateAPI.render(
+        "myappointments.html", response, {"title": "My Appointments", "apps":apps, "usertype": usertype}))
+
+@loginCheck
+def appointmentDelete(response):
+    result = db.deleteApp(dbConn, response.get_field('id'))
+    if result:
+        response.redirect("/myappointments")
+    else:
+        response.redirect("/dashboard?fail=1")
+
+@loginCheck
+def appointmentEdit(response):
+    app = {}
+    app["id"] = response.get_field('id', '')
+    app["title"] = response.get_field("title")
+    app["desc"] = response.get_field("desc")
+    app["location"] = response.get_field("location")
+    app["datetime"] = datetime.datetime.now().isoformat()
+    app["userid"] = response.get_secure_cookie('user_id')
+    app["active"] = 1
+    db.editApp(dbConn, app)
+    response.redirect("/myappointments")
 @loginCheck
 def test(response):
     response.write(TemplateAPI.render(
@@ -246,12 +357,19 @@ def main():
     server.register('/advertisement', advertisement,
                     get=advertisement, post=advertisementPost)
     server.register('/advertisement/delete', advertisementDelete)
+    server.register('/advertisement/view', adView)
+    server.register('/advertisement/edit', advertisementEdit)
     server.register('/resetpassword', resetPassword,
                     get=resetPassword, post=resetPasswordPost)
     server.register('/booking', booking, get=booking, post=bookingPost)
     server.register('/account', manageAccount,
                     get=manageAccount, post=editAccount)
     server.register('/myadvertisements', userAds)
+    server.register('/myappointments', viewAppointments)
+    server.register('/Appointment/delete', appointmentDelete)
+    server.register('/Appointment/edit', appointmentEdit)
+    server.register('/searchCharities', searchCharities)
+    server.register('/confirmation', confirmation)
     server.run(setup)
 
 
